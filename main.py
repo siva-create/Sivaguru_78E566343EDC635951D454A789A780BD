5c7c64fc7bdf357d50a61223ef75bffcc93d6b5f
siva-create <72457890+siva-create@users.noreply.github.com>
2023-10-05 14:48:15 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
-# 1.1 Implement a recursive function to calcute the factorial of a given number.
+'''Implement a class called BankAccount that represents a bank account. The class should have private 
+attributes for account number, account holder name, and account balance. Include methods to
+deposit money, withdraw money, and display the account balance. Ensure that the account balance
+cannot be accessed directly from outside the class. Write a program to create an instance of the 
+BankAccount class and test the deposit and withdrawal functionality.'''
 
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
 
-number = 2
-res = fact_rec(number)
+class BankAccount:
+  
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+    
+  def deposit(self, amount):
+   if amount > 0:
+     self.__account_balance += amount
+     # self.__account_balance = self.__account_bakance+amount 
+     print("Deposited ₹{}. New balance: ₹{}". format(amount,
+                                                    self.__account_balance))
+   else:
+     print("Invalid deposit amount.")
+     
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      # self.__ account _balance - amount 
+      print("Withdraw ₹{}". format (amount,
+                                   self.__account_balance))
+    else:
+       print("Invalid withdrawal amount or insufficient balance.")
 
-print("the factorial of {} is {}".format(number,res))
+  def display_balance(self):
+    print("Account balance for {} (Account #{}): ₹{}".format(
+        self.__account_holder_name, self.__account_number,
+        self.__account_balance ))
+
+
+# creat all instance of the Bankaccount class
+account = BankAccount(account_number="123456789",
+           account_holder_name="Hari Prabu",
+                     initial_balance=5000.0)
+
+# Test deposit and withdrawal functionality
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.display_balance()
+
+main (3).py
+Displaying main (3).py.
